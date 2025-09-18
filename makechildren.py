90530cdd6da949630f90b184a9f160323807cf70
alvapet04 <alvapet@kth.se>
2025-09-18 11:29:11 +0200

    Add files via upload

--- makechildren.py
+++ makechildren.py
@@ -2,11 +2,10 @@ from bintreeFile import Bintree
 from linkedQFile import LinkedQ
 
 
-class Node():
-
-    def __init__(self, x, next=None):
-        self.value = x
-        self.next = next
+class ParentNode:
+    def __init__(self, word, parent = None):
+        self.word = word
+        self.parent = parent
 
 def alla_ord ():
     svenska = Bintree()
@@ -18,46 +17,66 @@ def alla_ord ():
                 svenska.put(ordet)             # in i sökträdet
     return svenska, gamla
 
-def make_children(start_kö, ordträd, gamla):
+def make_children(start_kö, ordträd, gamla, slutord):
     alfabetet = "abcdefghijklmnopqrstuvwxyzåäö"
     startord = start_kö.dequeue()
-    gamla.put(startord)
-    for a in range(len(startord)):
+    gamla.put(startord.word)
+    for a in range(len(startord.word)):
         for symbol in alfabetet:
-            vårt_ord =list(startord)
+            vårt_ord =list(startord.word)
             vårt_ord[a] = symbol
             vårt_ord = "".join(vårt_ord)
             if vårt_ord in ordträd and vårt_ord not in gamla:
+                    #if vårt_ord == slutord:
+                      #   ord = (ParentNode(slutord, startord))   
+                     #    return ord   
                     gamla.put(vårt_ord)
+                    vårt_ord = ParentNode(vårt_ord, startord)
                     start_kö.enqueue(vårt_ord)       
     return start_kö
                 
                  
 def fråga_om_ord():
-    startord = input(f"Vilket är ditt startord?")
-    slutord = input(f"Vilket är ditt slutord?")
+    startord = input(f"Vilket är ditt startord?").strip()
+    slutord = input(f"Vilket är ditt slutord?").strip()
     return startord, slutord
+
 
 def utskrift(start_kö, svenska, slutord, gamla):
         if start_kö.isEmpty():
              print(f"Det finns ingen väg till {slutord}")
-             return
-        word = start_kö.first.value
-        if  word == slutord:
+             return None
+        ord = start_kö.first.value
+        if  ord.word == slutord:
                 print (f"Det finns en väg till {slutord}")
-                return 
+                return start_kö.first.value
         else:
-
-            start_kö = make_children(start_kö, svenska, gamla)
-            utskrift(start_kö, svenska, slutord, gamla)
+            start_kö = make_children(start_kö, svenska, gamla, slutord)
+            return utskrift(start_kö, svenska, slutord, gamla)
     
+
+def hela_utskriften(slutord):
+    if slutord != None:
+        hela_utskriften(slutord.parent)
+        print(slutord.word)
+     
 
 def main():
     svenska, gamla = alla_ord()
     startord, slutord = fråga_om_ord()
+    if startord not in svenska:
+         print("Ditt startord finns inte i ordlistan!")
+         return
+    elif slutord not in svenska:
+         print("Ditt slutord finns inte i ordlistan!")
+         return
     start_kö = LinkedQ()
+    startord = ParentNode(startord)
     start_kö.enqueue(startord)
-    utskrift(start_kö, svenska, slutord, gamla)
+    slutord = utskrift(start_kö, svenska, slutord, gamla)
+    if slutord != None:
+        hela_utskriften(slutord)
+    
     
 main()
 
